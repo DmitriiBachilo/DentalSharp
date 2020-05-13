6654e34d257c6d3ad5b409f82406100bdab93024
DmitriiBachilo <dimamn7@gmail.com>
2020-05-13 18:49:03 +0300

    Add project files.

---
+++ DentalSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DentalSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
