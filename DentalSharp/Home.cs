54bdf8178a10d30763cdfa3abc6423b4cc68e014
DmitriiBachilo <dimamn7@gmail.com>
2020-05-13 19:28:00 +0300

    Add Home screen

---
+++ DentalSharp/Home.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DentalSharp
+{
+    class Home
+    {
+        public void Display()
+        {
+            Console.WriteLine("Вас приветствует стоматологическая клиника Dental# \n");
+            Console.WriteLine("ВАШИ ЗУБЫ — НАША ЗАБОТА\n");
+            Console.WriteLine($"Локальное время: {DateTime.Now}");
+        }
+    }
+}
--- DentalSharp/Program.cs
+++ DentalSharp/Program.cs
@@ -6,7 +6,8 @@ namespace DentalSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Home home = new Home();
+            home.Display();
         }
     }
 }
